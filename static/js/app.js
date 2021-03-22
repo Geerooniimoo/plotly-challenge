9205d407d031cc0469399979fe8dc6eb0c113aba
Geronimo Perez <geronimo.perez4@gmail.com>
2021-03-22 14:26:46 -0400

    updated dashboard

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,13 @@
 
-d3.json('samples.json').then(data => {
-    var { names } = data;
-    names.forEach(name => d3.select('select').append('option').text(name));
-});
-
+displayOptions();
 showData();
+
+function displayOptions() {
+    d3.json('samples.json').then(data => {
+        var { names } = data;
+        names.forEach(name => d3.select('select').append('option').text(name));
+    });
+};
 
 function showData() {
     d3.json('samples.json').then(data => {
@@ -20,7 +23,7 @@ function showData() {
         var { otu_ids, sample_values, otu_labels } = samples;
 
         var barData = [{y: otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(), x:sample_values.slice(0,10).reverse(), text:otu_labels.slice(0,10).reverse(), type:'bar', orientation:'h'}];
-        var barLayout = {title: 'Top 10 Bacteria Cultures Found', margin: { t: 140, l: 150 }};
+        var barLayout = {title: {text: '<b>Top 10 Bacteria Cultures Found</b>','y':0.8},titlefont:{size:20}, margin: { t: 140, l: 150 }};
         Plotly.newPlot('bar', barData, barLayout);
 
         var bubbleData = [{y: otu_ids, x:sample_values, text:otu_labels, mode:'markers', marker:{size:sample_values, color:otu_ids, colorscale:'Earth'}}];
@@ -28,9 +31,8 @@ function showData() {
             xaxis:{title:'OTU ID'}, margin: {t:30}};
         Plotly.newPlot('bubble', bubbleData, bubbleLayout);
 
-        var gaugeData = [{domain:{x:[0,1], y:[0,1]}, value:metadata.wfreq, title: { text: "Belly Button Washing Frequency <br> Scrubs per Week" }, type:"indicator", mode:"gauge+number", delta:{reference:400}, gauge:{axis:{range:[0,9]}}}];
-        var layout = {width:600, height:400};
-        Plotly.newPlot('gauge', gaugeData, layout);
+        var gaugeData = [{domain:{x:[0,1], y:[0,1]}, value:metadata.wfreq, title: { text: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week" }, type:"indicator", mode:"gauge+number", delta:{reference:400}, gauge:{axis:{range:[0,9]}}}];
+        Plotly.newPlot('gauge', gaugeData, {autosize:true});
     });
 };
 
